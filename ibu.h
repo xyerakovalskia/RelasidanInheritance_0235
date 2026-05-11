89f64b335430658df7ef6c7a3320fa3c2c50aa22
xyerakovalskia <xyera123987@gmail.com>
2026-05-11 18:36:31 +0700

    menambahkan deklarasi dan construktor

--- ibu.h
+++ ibu.h
@@ -6,3 +6,20 @@ class anak;
 //membuat class ibu pada file header ibu.h
 class Ibu {
 public:
+    //deklarasi member variabel pada class ibu
+    string nama;
+    vector<anak*> daftar_anak;
+
+    //membuat constructor dan destructor pada class ibu
+    Ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    };
+
+    ~Ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    };
+
+    //deklarasi prosedur tambahAnak() dan cetakAnak()
+    void tambahAnak(anak* pAnak);
+    void cetakAnak();
+};
