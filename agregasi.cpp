d3d4bb391ccc459ceda01d0330cf22056a87a2d0
xyerakovalskia <xyera123987@gmail.com>
2026-05-11 18:40:16 +0700

    pemanggil dan pemberian argument pada prosedur

--- agregasi.cpp
+++ agregasi.cpp
@@ -15,3 +15,8 @@ int main()
     anak* varAnak1 = new anak("tono");
     anak* varAnak2 = new anak("tiara");
     anak* varAnak3 = new anak("dini");
+
+    //pemanggilan dan pemberian argument pada prosedur tambahAnak()
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
