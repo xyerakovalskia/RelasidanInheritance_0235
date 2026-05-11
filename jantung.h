bf37290ef2ce651c04ebb74cf347fe308162d6f1
xyerakovalskia <xyera123987@gmail.com>
2026-05-11 18:44:11 +0700

    pembukaan

--- jantung.h
+++ jantung.h
@@ -1,16 +1,2 @@
 #ifndef JANTUNG_H
 #define JANTUNG_H
-
-//membuat class jantung di file header jantung.h
-class Jantung {
-public:
-    //membuat constructor dan destructor dari class jantung
-    Jantung() {
-        cout << "Jantung Ditambah\n";
-    }
-    ~Jantung() {
-        cout << "Jantung Dimatikan\n";
-    }
-};
-
-#endif // !JANTUNG_H
