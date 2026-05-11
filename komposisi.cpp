826f5d658fde06c43a4de6a512fbecd021c156d4
xyerakovalskia <xyera123987@gmail.com>
2026-05-11 18:48:55 +0700

    mendelete pointer

--- komposisi.cpp
+++ komposisi.cpp
@@ -10,3 +10,7 @@ int main()
 {
     //deklarasi dan pemberian nilai object pointer varManusia
     Manusia* varManusia = new Manusia("jono");
+    //mendelete pointer varManusia dari memori
+    delete varManusia;
+    return 0;
+}
