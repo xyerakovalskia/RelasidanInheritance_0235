19c88664bd2e259a6cac6da1ca2142193e91e45f
xyerakovalskia <xyera123987@gmail.com>
2026-05-11 18:28:43 +0700

    membuat class anak di file header anak.h

--- anak.h
+++ anak.h
@@ -3,16 +3,3 @@
 //membuat class anak di file header anak.h
 class anak {
 public:
-    //deklarasi member variabel pada class anak
-    string nama;
-
-    //membuat constructor dan destructor pada class anak
-    anak(string pNama) : nama(pNama) {
-        cout << "Anak \"" << nama << "\" ada \n";
-    }
-
-    ~anak() {
-        cout << "Anak \"" << nama << "\" tidak ada\n";
-    }
-};
-#endif
