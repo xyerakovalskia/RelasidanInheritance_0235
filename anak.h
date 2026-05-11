1423e5ae2ab789412ef7f9e85cd9ca0e86ed8faf
xyerakovalskia <xyera123987@gmail.com>
2026-05-11 18:30:50 +0700

    membuat construktor dan destructor pada class anak

--- anak.h
+++ anak.h
@@ -5,3 +5,14 @@ class anak {
 public:
     //deklarasi member variabel pada class anak
     string nama;
+
+    //membuat constructor dan destructor pada class anak
+    anak(string pNama) : nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada \n";
+    }
+
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+#endif
