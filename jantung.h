94ed8b8d5171a6b99036eaf522123dd56a18f09f
xyerakovalskia <xyera123987@gmail.com>
2026-05-11 18:45:04 +0700

    membuat constructor dan destructor

--- jantung.h
+++ jantung.h
@@ -4,3 +4,13 @@
 //membuat class jantung di file header jantung.h
 class Jantung {
 public:
+    //membuat constructor dan destructor dari class jantung
+    Jantung() {
+        cout << "Jantung Ditambah\n";
+    }
+    ~Jantung() {
+        cout << "Jantung Dimatikan\n";
+    }
+};
+
+#endif // !JANTUNG_H
