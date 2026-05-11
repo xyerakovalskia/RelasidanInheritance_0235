f77cb1e9286023ab07da3699e2954c300279d496
xyerakovalskia <xyera123987@gmail.com>
2026-05-11 18:43:03 +0700

    pembuatan class dokter

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -26,3 +26,25 @@ public:
     void cetakDokter();
 
 };
+
+//pembuatan class dokter
+class dokter {
+public:
+    //deklarasi member variabel dari class dokter
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    //pembuatan constructor dan destructor dari class dokter
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    //deklarasi prosedur tambahPasien() dan cetakPasien()
+    void tambahPasien(pasien*);
+    void cetakPasien();
+
+};
