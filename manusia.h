977bab9562ec699060d938bc472762311c5136db
xyerakovalskia <xyera123987@gmail.com>
2026-05-11 18:45:38 +0700

    membuat class manusia

--- manusia.h
+++ manusia.h
@@ -3,16 +3,3 @@
 //membuat class manusia di file header manusia.h
 class Manusia {
 public:
-    //membuat member variabel class manusia
-    string name;
-    Jantung varJantung;
-
-    //membuat constructor dan destructor class manusia
-    Manusia(string pName) : name(pName) {
-        cout << name << "Hidup\n";
-    }
-    ~Manusia() {
-        cout << name << "Mati\n";
-    }
-};
-#endif // !MANUSIA_H
