aa5784903beae188ac32b980e7c74799c16c15f7
xyerakovalskia <xyera123987@gmail.com>
2026-05-11 18:49:46 +0700

    pembuatan class orang

--- inheritance.cpp
+++ inheritance.cpp
@@ -23,37 +23,3 @@ public:
     }
 
 };
-
-//pembuatan class pelajar sebagai pewaris dari class orang
-class pelajar : public orang {
-public:
-    //deklarasi member variabel dari class pelajar
-    string sekolah;
-
-    //pembuatan constructor dan destructor dari class pelajar
-    pelajar(string pNama, string pSekolah) :orang(pNama), sekolah(pSekolah) {
-        cout << "Pelajar dibuat\n" << endl;
-    }
-
-    ~pelajar() {
-        cout << "Pelajar dihapus\n" << endl;
-    }
-
-    //pembuatan function perkenalan()
-    string perkenalan() {
-        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
-    }
-
-};
-
-int main()
-{
-    //deklarasi dan pemberian nilai untuk variabel object siswa1
-    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
-    //pemanggilan function perkenalan dari class pelajar
-    cout << siswa1.perkenalan();
-    //pemanggilan function warisan dari class orang kepada class object
-    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
-
-    return 0;
-}
